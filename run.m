54502785b1a31bff68771e985d734f0d989d1ebf
Federico Betti <74817555+federicobetti99@users.noreply.github.com>
2022-11-02 11:29:31 +0100

    Create run.m

---
+++ run.m
@@ -0,0 +1,84 @@
+%% clean
+clear
+close all
+clc
+
+%% define Hilbert matrix
+n = 100;
+A = hilb(n);
+
+%% useful quantities
+num_avg = 20;
+lambda = 1e-4;
+[U, S, V] = svd(A);
+gold_standards = diag(S);
+
+
+%% ridge leverage scoring
+errors_ridge = zeros(num_avg, 50);  % initialize errors vector
+for i=1:num_avg
+    for j=1:50
+       ridge_scores = zeros(n, 1);
+       for k=1:n
+           ridge_scores(k) = V(k, :) * diag(diag(S).^2 ./ (diag(S).^2 + lambda^2)) * V(k, :)';
+       end
+       ridge_scores = ridge_scores / sum(ridge_scores);
+       indeces = randsample(n, j, true, ridge_scores);
+       B = A(:, indeces);
+       errors_ridge(i, j) = norm(A-B*pinv(B)*A, 2);
+    end
+end
+mean_errors_ridge = mean(errors_ridge);
+std_errors_ridge = std(errors_ridge) / sqrt(num_avg);
+
+%% uniform sampling
+errors_uniform = zeros(num_avg, 50);  % initialize errors vector
+B = A;  % copy of A for replacement
+for i=1:num_avg
+    for j=1:50
+       indeces = randsample(n, j, true);
+       B = A(:, indeces);
+       errors_uniform(i, j) = norm(A-B*pinv(B)*A, 2);
+    end
+end
+mean_errors_uniform = mean(errors_uniform);
+std_errors_uniform = std(errors_uniform);
+
+%% proportionally to the rows of V
+errors_columns = zeros(num_avg, 50);  % initialize errors vector
+for i=1:num_avg
+    for j=1:50
+       columns_scores = zeros(n, 1);
+       for k=1:n
+           column_scores(k) = norm(A(:, k), 2) / norm(A, "fro");
+       end
+       indeces = randsample(n, j, true, column_scores);
+       B = A(:, indeces);
+       errors_columns(i, j) = norm(A-B*pinv(B)*A, 2);
+    end
+end
+mean_errors_columns = mean(errors_columns);
+std_errors_columns = std(errors_columns);
+
+%% plot results
+figure()
+x = (1:50);
+semilogy(x, mean_errors_ridge, 'LineWidth', 2.5);
+hold on
+patch([x fliplr(x)], [mean_errors_ridge-std_errors_ridge  fliplr(mean_errors_ridge+std_errors_ridge)], 'r', 'FaceAlpha',0.2, 'EdgeColor', 'Blue');
+hold on
+semilogy(x, mean_errors_columns, 'LineWidth', 2.5);
+hold on
+patch([x fliplr(x)], [mean_errors_columns-std_errors_columns fliplr(mean_errors_columns+std_errors_columns)], 'r', 'FaceAlpha', 0.2, 'EdgeColor','Red');
+hold on
+semilogy((1:50), mean_errors_uniform, 'LineWidth', 2.5);
+hold on
+patch([x fliplr(x)], [mean_errors_uniform-std_errors_uniform fliplr(mean_errors_uniform+std_errors_uniform)], 'r', 'FaceAlpha', 0.2, 'EdgeColor', 'Yellow');
+xlabel("k", 'FontSize', 20);
+hold on
+semilogy(x, gold_standards(2:51), 'LineWidth', 1.5);
+ylabel("$\vert \vert A - C C^\dagger A \vert \vert_2$", 'interpreter', 'latex', 'FontSize', 20);
+title("Low-rank approximation by column sampling", 'FontSize', 30);
+legend("Ridge leverage scores", "", "Columns norm sampling", "", ...
+       "Uniform sampling", "", "$\sigma_{k+1}(A)$", 'interpreter', 'latex');
+legend('Location', 'southoutside', 'FontSize', 20, 'NumColumns', 4);
