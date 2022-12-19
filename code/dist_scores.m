1acf31db1d7639438fc4ed9cb48b664b07183118
Federico Betti <74817555+federicobetti99@users.noreply.github.com>
2022-12-19 16:43:41 +0100

    Added missing script for distribution of scores and added it in main.m

---
+++ code/dist_scores.m
@@ -0,0 +1,37 @@
+%% clean
+clear
+close all
+clc
+
+%% define Hilbert matrix and compute all scores for sketching
+n = 100;
+A = hilb(n);
+[U, S, V] = svd(A);
+
+fig_legend_string = ["$\propto l_{i, \lambda}(A)$", "$\propto 1/n$", ...
+                     "$\propto \| a_j \|_2^2$"];
+
+lambda = 1e-4; % regularization parameter
+ridge_scores = diag(V * diag(diag(S).^2 ./ (diag(S).^2 + lambda^2)) * V'); % ridge scores
+ridge_scores = ridge_scores / sum(ridge_scores);
+
+column_scores = sum(A.^2, 2) / norm(A, "fro")^2;  % column scores
+
+uniform_scores = 1/n * ones(n, 1);  % uniform scores
+
+%% plot cumulative sums
+fig = figure();
+plot(cumsum(ridge_scores), 'LineWidth', 2.5);
+hold on
+plot(cumsum(uniform_scores), 'LineWidth', 2.5);
+hold on
+plot(cumsum(column_scores), 'LineWidth', 2.5);
+xlabel("Column index", 'FontSize', 12);
+ylabel("Cumulative sum", 'FontSize', 12);
+ax = gca;
+ax.XAxis.FontSize = 14;
+ax.YAxis.FontSize = 14;
+title("Cumulative sum of sketching distributions", 'FontSize', 12);
+legend(fig_legend_string, 'interpreter', 'latex');
+legend('Location', 'southeast', 'FontSize', 15, 'NumColumns', 1);
+saveas(fig, "../figures/dist_scores", "epsc");
--- code/main.m
+++ code/main.m
@@ -3,9 +3,17 @@ clear
 close all
 clc
 
+% Running the three subsections below reproduces the plots shown in
+% the report in the same order as they appear in the latter
+
 %% run comparison between uniform sampling, columns norm sampling and ridge scores
 tic
 comparison
+toc
+
+%% plot cumulative sum of sketching distributions
+tic
+dist_scores
 toc
 
 %% run comparison between rows of Vk norm sampling and ridge scores with adaptive lambda
