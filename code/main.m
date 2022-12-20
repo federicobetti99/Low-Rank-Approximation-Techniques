65ee8dd08a6be450d1888173c6a5ef6a3e5a10c6
Federico Betti <74817555+federicobetti99@users.noreply.github.com>
2022-12-20 16:52:59 +0100

    Check of reproducibility ok

--- code/main.m
+++ code/main.m
@@ -7,16 +7,10 @@ clc
 % the report in the same order as they appear in the latter
 
 %% run comparison between uniform sampling, columns norm sampling and ridge scores
-tic
 comparison
-toc
 
 %% plot cumulative sum of sketching distributions
-tic
 dist_scores
-toc
 
 %% run comparison between rows of Vk norm sampling and ridge scores with adaptive lambda
-tic
 lambda_tuning
-toc
